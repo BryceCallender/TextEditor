ddf057b0ef8436b80c43b3a93a31c41080f32d80
Bryce Callender <brycec848@gmail.com>
2020-02-24 16:26:28 -0800

    Code syntax highlighter start

---
+++ TextEditor/codesyntaxhighligher.cpp
@@ -0,0 +1,6 @@
+#include "codesyntaxhighligher.h"
+
+CodeSyntaxHighligher::CodeSyntaxHighligher()
+{
+
+}
---
+++ TextEditor/codesyntaxhighligher.h
@@ -0,0 +1,12 @@
+#ifndef CODESYNTAXHIGHLIGHER_H
+#define CODESYNTAXHIGHLIGHER_H
+
+
+class CodeSyntaxHighligher
+{
+    Q_OBJECT
+public:
+    CodeSyntaxHighligher();
+};
+
+#endif // CODESYNTAXHIGHLIGHER_H
