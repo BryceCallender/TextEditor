e21413c5c69e671844afcd8e5984b37afb1a7b6a
Bryce <brycec848@gmail.com>
2020-03-08 22:27:35 -0700

    Code color changes on setting window close

--- TextEditor/codesyntaxhighlighter.cpp
+++ TextEditor/codesyntaxhighlighter.cpp
@@ -7,14 +7,29 @@ CodeSyntaxHighlighter::CodeSyntaxHighlighter(QTextDocument *parent): QSyntaxHigh
 
 void CodeSyntaxHighlighter::updateKeywordColor(const QColor &color)
 {
+    for(int i = 0; i < highlightingRules.size(); i++)
+    {
+        HighlightingRule rule = highlightingRules.at(i);
+
+        if(rule.format == keywordFormat)
+        {
+            rule.format.setForeground(color);
+            rule.format.setFontWeight(QFont::Bold);
+
+            qDebug() << "Set" << rule.pattern.pattern() << "to color" << color;
+        }
+
+        highlightingRules[i] = rule;
+    }
+
     keywordFormat.setForeground(color);
+
     rehighlight();
     qDebug() << "New Color: " << color;
 }
 
 void CodeSyntaxHighlighter::highlightBlock(const QString &text)
 {
-    qDebug() << "Highlighting...";
     for (const HighlightingRule &rule : qAsConst(highlightingRules))
     {
         QRegularExpressionMatchIterator matchIterator = rule.pattern.globalMatch(text);
