4601829a1c9d4823920991bc20ff33ad8b399ba2
Bryce <brycec848@gmail.com>
2020-03-29 16:17:36 -0700

    Pre Spring break finalized edits

--- TextEditor/customdockwidget.cpp
+++ TextEditor/customdockwidget.cpp
@@ -11,14 +11,15 @@ void CustomDockWidget::closeEvent(QCloseEvent *event)
     MainWindow* mainWindow = reinterpret_cast<MainWindow*>(parentWidget());
 
     QVector<CustomTabWidget*> tabWidget = mainWindow->getTabWidgets();
+    int tabIndex = findChild<CustomTabWidget*>()->tabWidgetIndex;
 
     int count = 0;
-    for(int i = 0; i < tabWidget.at(CustomTabWidget::currentSelectedTabIndex)->count(); i++)
+    for(int i = 0; i < tabWidget.at(tabIndex)->count(); i++)
     {
-        if(tabWidget.at(CustomTabWidget::currentSelectedTabIndex)->tabText(i).back() == '*')
+        if(tabWidget.at(tabIndex)->tabText(i).back() == '*')
         {
             QString message, temp;
-            if(tabWidget.at(CustomTabWidget::currentSelectedTabIndex)->tabText(i).left(tabWidget.at(CustomTabWidget::currentSelectedTabIndex)->tabText(i).size() - 1) == "New Tab")
+            if(tabWidget.at(tabIndex)->tabText(i).left(tabWidget.at(tabIndex)->tabText(i).size() - 1) == "New Tab")
             {
                 temp.setNum(count + 1);
                 count++;
@@ -26,31 +27,28 @@ void CustomDockWidget::closeEvent(QCloseEvent *event)
             }
             else
             {
-                message = tabWidget.at(CustomTabWidget::currentSelectedTabIndex)->tabText(i).left(tabWidget.at(CustomTabWidget::currentSelectedTabIndex)->tabText(i).size() - 1);
+                message = tabWidget.at(tabIndex)->tabText(i).left(tabWidget.at(tabIndex)->tabText(i).size() - 1);
             }
             int clicked = QMessageBox::warning(this, "Save?", "Would you like to save " + message + "?", QMessageBox::Ok ,QMessageBox::No);
             if(clicked == QMessageBox::Ok)
             {
                 mainWindow->save();
                 qDebug() << "Closing " + QString::number(i);
-                tabWidget.at(CustomTabWidget::currentSelectedTabIndex)->removeTab(i);
+                tabWidget.at(tabIndex)->removeTab(i);
                 i--;
             }
             else if(clicked == QMessageBox::No)
             {
                 qDebug() << "Closing " + QString::number(i);
-                tabWidget.at(CustomTabWidget::currentSelectedTabIndex)->removeTab(i);
+                tabWidget.at(tabIndex)->removeTab(i);
                 i--;
             }
         }
-        else if(tabWidget.at(CustomTabWidget::currentSelectedTabIndex)->tabText(i) != "+")
+        else if(tabWidget.at(tabIndex)->tabText(i) != "+")
         {
-            tabWidget.at(CustomTabWidget::currentSelectedTabIndex)->removeTab(i);
+            tabWidget.at(tabIndex)->removeTab(i);
             i--;
         }
     }
-
-
-
 }
 
